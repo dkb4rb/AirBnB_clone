a7c7be43f0ccac020dd5d4ec688348b33d3ccfbd
dkb4rb <jjuannca651@gmail.com>
2021-11-10 02:42:29 -0500

    Appen new Base Models

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Import Libraries and moduls
+"""
+import models
+from time import time
+from uuid import uuid4
+from datetime import datetime
+from time import sleep
+
+
+class BaseModel:
+    """ Represent new class BaseModel
+    """
+    def __init__(self, *args, **kwargs):
+        """ Instantiation BaseModel
+        """
+
+        date = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for v, f in kwargs.items():
+                if v == "created_at" or v == "update_at":
+                    self.__dict__[v] = datetime.strptime(f, date)
+                else:
+                    self.__dict__[v] = f
+
+    def save(self):
+        """ Update the current datetime"""
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """ Return the dictionary of the BaseModel"""
+        dictionary = self.__dict__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary["__class__"] = self.__class__.__name__
+        return dictionary
+
+    def __str__(self):
+        """Return The print in str the representation
+        of the Instance BaseModel"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
