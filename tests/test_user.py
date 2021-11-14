a4ee89add0d805d0b68db7161f9171a5b453f4dd
lisethav <lisethav@gmail.com>
2021-11-14 01:19:20 -0500

    test user

---
+++ tests/test_user.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import unittest
+import pep8
+from models.base_model import BaseModel
+from models.user import User
+
+class test_Requirements_User(unittest.TestCase):
+    """ Unittest to Holberton Requirements """
+
+    def test_shebang_user(self):
+        shebang = "#!/usr/bin/python3"
+        f = open("tests/test_base_model.py", "r")
+        for linea in f:
+            if shebang == linea:
+                print(shebang)
+        f.close()
+
+    def test_pep8_conformance_user(self):
+        fchecker = pep8.Checker('models/user.py', show_source=True)
+        file_errors = fchecker.check_all()
+
+        def __str__(self):
+            print("\n")
+
+class TestUser(unittest.TestCase):
+    """Testing class user from models"""
+
+    def test_insistance_user(self):
+        """Test if the data entered is user type"""
+        item = User()
+        self.assertTrue(isinstance(item, BaseModel))
+
+    def test_email(self):
+        """Test if it has email attribute"""
+        item = User()
+        self.assertTrue(hasattr(item, "email"))
+
+    def test_password(self):
+        """Test if it has password attribute"""
+        item = User()
+        self.assertTrue(hasattr(item, "password"))
+ 
+    def first_name(self):
+        """Test if it has password attribute"""
+        item = User()
+        self.assertTrue(hasattr(item, "first_name"))
+
+    def last_name(self):
+        """Test if it has password attribute"""
+        item = User()
+        self.assertTrue(hasattr(item, "last_name"))
+ 
+if __name__ == "__main__":
+    unittest.main()
