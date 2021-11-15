c8a1cf8932a5e0873b9bea5d9d455b9604e955bf
lisethav <lisethav@gmail.com>
2021-11-14 22:13:39 -0500

    comment

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Test for base model
+"""
 import unittest
 import os
 from datetime import datetime
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Test for user
+"""
 import unittest
 from models.base_model import BaseModel
 from models.user import User
