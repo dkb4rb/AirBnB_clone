a44fa02fb9019a069b92192d0714a9b9ad0d2082
Juan Duque <84897255+dkb4rb@users.noreply.github.com>
2021-11-14 22:48:19 -0500

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,8 +2,11 @@
 """
 Test for place
 """
+import os
+import models
 import unittest
-from models.base_model import BaseModel
+from datetime import datetime
+from time import sleep
 from models.place import Place
 
 
